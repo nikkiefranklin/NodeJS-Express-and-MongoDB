7f3731f065066bf78875dceb61661de29a76ba3a
Nicole <nikkiefranklin@yahoo.com>
2021-06-26 10:32:41 -0400

    Node MongoDB Example 1

--- node-express/routes/campsiteRouter.js
+++ node-express/routes/campsiteRouter.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const campsiteRouter = express.Router();
+
+campsiteRouter.use(bodyParser.json());
+
 
 campsiteRouter.route('/')
 .all((req, res, next) => {
@@ -20,5 +25,28 @@ campsiteRouter.route('/')
 .delete((req, res) => {
     res.end('Deleting all campsites');
 });
+
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader = ('Content-Type', 'text/plain');
+  next();
+})
+.get((req, res) => {
+  res.end(`Will send details of campsite ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+  res.statusCode = 403;
+  res.end(`Post operation not supported on /campsites/${req.params.campsiteId}`);
+})
+.put((req, res) => {
+  res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+  res.end(`Will update the campsite: ${req.body.name}
+      with description: ${req.body.description}`);
+})
+.delete((req, res) => {
+  res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
+
 
 module.exports = campsiteRouter;
--- node-express/server.js
+++ node-express/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const campsiteRouter = require('./routes/campsiteRouter');
+const campsiteRouter = require('./routes/campsiteRouter'); 
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -12,6 +13,9 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
+
 
 app.use(express.static(__dirname + '/public'));
 
