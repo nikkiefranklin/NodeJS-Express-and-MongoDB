bbe6aca15aa02075cc7f0660d74cf1632679b714
Nicole <nikkiefranklin@yahoo.com>
2021-07-07 01:37:16 -0400

    Gitignore

--- nucampsiteServer/config.js
+++ nucampsiteServer/config.js
@@ -1,8 +1,4 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '1019549898791228',
-        clientSecret: 'fda23186f6eacf1207c1e5097e9270bf'
-    }
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
 }
