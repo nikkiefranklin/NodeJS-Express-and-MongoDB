01cf8f21165b558c68550b55693985bc327b4780
Nicole <nikkiefranklin@yahoo.com>
2021-07-06 22:42:58 -0400

    Passport 2

--- nucampsiteServer/config.js
+++ nucampsiteServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '1019549898791228',
+        clientSecret: 'fda23186f6eacf1207c1e5097e9270bf'
+    }
 }
